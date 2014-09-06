472206bae3ae3227b01010c5c72a0bb47a75b6e8
Anton Krasnokutskiy <krasnokutskiy.anton@gmail.com>
2014-09-07 00:34:46 +0400

    DB settings & package.json

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
 module.exports = {
-    httpPort: 8000
+  httpPort: 8000,
+  db_config: {
+    db_adress: "192.168.0.102",
+    db_port: "3306",
+    user_name: "nagibator2005",
+    password: "Fhntv11995"
+  }
 };
--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var config = require("./config");
 var route = require("./route").route;
 
 function startServer(route) {
-    function onRequest(request, response) {
-        route(request, response);
-    }
-    http.createServer(onRequest).listen(config.httpPort);
-    console.log("Server has started, " + (new Date().toString()));
+  function onRequest(request, response) {
+    route(request, response);
+  }
+  http.createServer(onRequest).listen(config.httpPort);
+  console.log("Server has started, " + (new Date().toString()));
 }
 
 startServer(route);
